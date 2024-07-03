7aa358e079e5baae4870d44716975606f6620141
Nang <nathanang2000@gmail.com>
2024-07-02 20:22:43 -0400

    Added monica's changes p1

--- assets/questions/questions.js
+++ assets/questions/questions.js
@@ -1,146 +1,146 @@
 const questions = [
   {
-    "question": "Q1/12: You're taking the bus on your way back home. It's been a loooong day, what are you thinking about?",
+    "question": "Q1: You’ve landed in a vast, outlandish field surrounded by many things/creatures completely unfamiliar to you, what do you want to do first?",
     "image": "assets/questions/gifs/Q1.gif",
     "answers": {
-      "F": {
-        "text": "I'm just so tired and can't wait to go home. Why does everyday feel the same...",
+      "E": {
+        "text": "You see a village just beyond the oddly shaped bridge. Let’s go talk to them and get an idea of the land.",
       },
-      "T": {
-        "text": "Should I rest or work first when I get home? Most importantly, what's for dinner?",
+      "I": {
+        "text": "You see the village, but you want to explore on your own first. There’s so many cool plants, animals, and architecture; you just can’t wait to explore them!",
       }
     }
   },
   {
-    "question": "Q2/12: You look outside the windows, seems like it's going to rain soon. Suddenly everything turns into darkness!!",
+    "question": "Q2: After visiting the village, you discover a secret within the realm related to a giant tree called the Elder. The villagers believe you were meant to save its future. What do you do?",
     "image": "assets/questions/gifs/Q2.gif",
     "answers": {
-      "J": {
-        "text": "I don't remember this tunnel. *Pull out your phone to look at the map*",
+      "N": {
+        "text": "Check out the tree yourself. Maybe you’ll make a connection with it.",
       },
-      "P": {
-        "text": "*Turn around to see the other's reaction* Should I ask the driver what's going on?",
+      "S": {
+        "text": "Check out the local library and talk to the researcher.",
       }
     }
   },
   {
-    "question": "Q3/12: Before you can do anything, the bus stops and everything around you floats up!",
+    "question": "Q3: With all this running around and stress from being away from your own world, you find yourself tired and exhausted. Let’s rest:",
     "image": "assets/questions/gifs/Q3.gif",
     "answers": {
-      "S": {
-        "text": "Ok this is weird, am I dreaming? *pinch your arm*",
+      "E": {
+        "text": "Go to the local tavern and unwind by connecting with new friends, before hitting the bed.",
       },
-      "N": {
-        "text": "Is this a prank? Am I getting kidnapped by aliens?!",
+      "I": {
+        "text": "Rest in a small pod near the river and take in the scenery.",
       }
     }
   },
   {
-    "question": "Q4/12: Amidst the surprise, your eyes catch something drifting past the window.",
+    "question": "Q4: You wake up to the cry of a creature in the distance. What are you thinking?",
     "image": "assets/questions/gifs/Q4.gif",
     "answers": {
-      "F": {
-        "text": "Woahhh...so many cats! How cute!! I wanna give them a hug!",
-      },
       "T": {
-        "text": "Cats? In space? That's kinda odd... How do they survive out there?!",
+        "text": "Sounds like danger. Run back to the village.",
+      },
+      "F": {
+        "text": "Oh no! You’re sure it needs help and want to carefully inspect the scene. You can’t move on without knowing if the creature will be okay.",
       }
     }
   },
   {
-    "question": "Q5/12: 'Welcome Aboard!' a small robot appears and announces, 'We will reach our destination in 5 months 6 days and 2 hours!'",
+    "question": "Q5: You start wandering again through this vast world and run into a group of travelers…",
     "image": "assets/questions/gifs/Q5.gif",
     "answers": {
-      "P": {
-        "text": "Cool! A space cruise?! Where are we going? Do I get a spacesuit? This is so exciting!",
-      },
       "J": {
-        "text": "Hey uh...Am I in space? How did I get here? When can I go home?! Why..",
+        "text": "How the hell are they carrying all that? How is that bag even holding up?",
+      },
+      "P": {
+        "text": "Wait, they’re kind of adorable?",
       }
     }
   },
   {
-    "question": "Q6/12: The robot just points to the back of the bus which now looks more like inside of a spaceship! What do you want to see first?",
+    "question": "Q6: Down the road, a creature with a golden glow bows before you leaving a trinket seemingly unremarkable.",
     "image": "assets/questions/gifs/Q6.gif",
     "answers": {
-      "I": {
-        "text": "Solar system themed fountain in the middle. I want to take pictures with it.",
+      "T": {
+        "text": "What just happened?... It looks like any normal object, but the creature looked so angelic. Let me take this to the local researcher. See what they think.",
       },
-      "E": {
-        "text": "Cozy corner with a massaging chair. I need to sit down and process.",
+      "F": {
+        "text": "What could this mean? I feel so reassured; this world truly believes in me.",
       }
     }
   },
   {
-    "question": "Q7/12: On the way back to your seat, you are surrounded by cats! 'Unauthorized passenger detected, CAPTURE CAPTURE!'",
+    "question": "Q7: The researcher you’ve been working closely with suddenly asks you for advice on their own curiosities.",
     "image": "assets/questions/gifs/Q7.gif",
     "answers": {
-      "S": {
-        "text": "Woah! What do you mean unauthorized? I'm the passenger of this cruise!",
-      },
-      "N": {
-        "text": "Ok, I have no idea what's going on. How about we calm down and chat for a bit?",
-      }
-    }
-  },
-  {
-    "question": "Q8/12: Suddenly, the robot glides in, it whispers something to the cats that makes them stop.",
-    "image": "assets/questions/gifs/Q8.gif",
-    "answers": {
-      "E": {
-        "text": "Bob!! Thank you! You come to save me, right?",
-      },
-      "I": {
-        "text": "HA! That's right! Back off, cats! Am I safe to go now?!",
-      }
-    }
-  },
-  {
-    "question": "Q9/12: The robot transforms into a giant vacuum cleaner, the cats use it to point at you!",
-    "image": "assets/questions/gifs/Q9.gif",
-    "answers": {
       "J": {
-        "text": "Throw your coat at them as distraction, run towards the big green 'EXIT' sign!",
+        "text": "Tell them what you would do in detail.",
       },
       "P": {
-        "text": "Run in random direction, confuse them, blend in with the crowd!",
+        "text": "You see a solution but consider so many other possibilities as well – talk to them about all of them. They’ll come to their own conclusions.",
       }
     }
   },
   {
-    "question": "Q10/12: You run past the gift shop. Looks like you got rid of them. It wouldn't hurt to get some souvenirs, right?",
+    "question": "Q8: You’re missing your loved ones back in your world. (Time hasn’t passed in your world but you don’t know that)",
+    "image": "assets/questions/gifs/Q8.gif",
+    "answers": {
+      "T": {
+        "text": "I was brought here for a reason, let’s get back to focusing on that. You must be patient and follow your plan.",
+      },
+      "F": {
+        "text": "They must be so worried, I should finish things here quickly. You try to find a creative but fast way to finish your quest.",
+      }
+    }
+  },
+  {
+    "question": "Q9: The state of the magical, giant tree is really starting to look grim. You’ve been really stuck on what to do.",
+    "image": "assets/questions/gifs/Q9.gif",
+    "answers": {
+      "N": {
+        "text": "You’re stressed but you already have a list of things you need to do in your head – thanks to your research.",
+      },
+      "S": {
+        "text": "You start to feel panicky but want to follow your gut feelings of what you need to do.",
+      }
+    }
+  },
+  {
+    "question": "Q10: You’re ready to save the Elder Tree. How do you prepare?",
     "image": "assets/questions/gifs/Q10.gif",
     "answers": {
-      "F": {
-        "text": "Get the Jupiter headphone, wear it to hear people's thoughts!",
+      "E": {
+        "text": "You find power in working with those you made connections with here. Let’s bring them along with you on your journey.",
       },
-      "T": {
-        "text": "Get the Mars watch, turn it and you can time travel!",
+      "I": {
+        "text": "You find knowledge in solitude. You’ve already talked to enough people about your gatherings.",
       }
     }
   },
   {
-    "question": "Q11/12: The giant vacuum cleaner suddenly emerges in front of you! You're sucked into its vortex!!!",
+    "question": "Q11: You reach the Elder Tree and place the “seemingly, unremarkable” trinket you were gifted earlier at its base. You’re soon enveloped in light.",
     "image": "assets/questions/gifs/Q11.gif",
     "answers": {
-      "S": {
-        "text": "It's ok, this is just a dream! I'll wake up soon! *close your eyes and give up*",
+      "J": {
+        "text": "No, wait! No one told me this would happen! WHAT’S GOING ON!",
       },
-      "N": {
-        "text": "I'll find the way out! Maybe there are some secret doors! *look around for the way out*",
+      "P": {
+        "text": "Whoa~ I don’t know what’s happening, but the light feels like a hug.",
       }
     }
   },
   {
-    "question": "Q12/12: After a moment of darkness, you find yourself back in the bus. 'Are you alright?' the ticket inspector asks",
+    "question": "Q12: You’re suddenly back to where you were in your own world. The trinket is back in your pocket but it now glows in memory with your adventure.",
     "image": "assets/questions/gifs/Q12.gif",
     "answers": {
-      "I": {
-        "text": "Woah! I thought I was captured... Nevermind! I'm alright now, I guess? *panic in silence*",
+      "N": {
+        "text": "Hug it tight",
       },
-      "E": {
-        "text": "You won't believe it...I think I just had the wildest dream ever! *tell him about your space adventure*",
+      "S": {
+        "text": "Gather yourself, 'I’m glad to be back, let’s put it somewhere safe'",
       }
     }
   }
-]
+];
